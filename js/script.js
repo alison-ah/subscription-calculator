b21de961fd89512d0b7da6f194c5ae7fff9919ec
Alison <alison@andersonholland.com>
2024-10-26 13:56:52 -0500

    Event listeners added

--- js/script.js
+++ js/script.js
@@ -4,3 +4,15 @@ var subDurationElement = document.querySelector("#months");
 var result = document.querySelector(".result");
 var subType = "basic";
 var subDuration = 1;
+
+subTypeElement.addEventListener("change", function (e) {
+    subType = e.target.value;
+    //console.log(subType);
+
+});
+
+subDurationElement.addEventListener("change", function (e) {
+    subType = e.target.value;
+    console.log(subType);
+    
+});
